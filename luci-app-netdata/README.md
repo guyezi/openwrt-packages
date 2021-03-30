572746817be016f27e15e8ed2c80ec54d9551075
guyezi <7312140@qq.com>
2021-03-30 17:03:08 +0000

    update

---
+++ luci-app-netdata/README.md
@@ -0,0 +1,18 @@
+# luci-app-netdata
+
+> 网络信息查看 netdata
+
+编译:
+
+```
+make package/luci-app-netdata/compile V=s
+```
+
+生成软件包:
+
+* luci-app-netdata
+* luci-i18n-netdata-zh-cn
+
+官方依赖库:
+
+* netdata
--- luci-app-ttnode/luasrc/model/cbi/ttnode/client.lua
+++ luci-app-ttnode/luasrc/model/cbi/ttnode/client.lua
@@ -13,7 +13,7 @@ local uci = luci.model.uci.cursor()
 local check_login = ttnode.getInitInfo()
 local isLogin = check_login.errCode == 0 and true or false
 
-m = Map(config, translate('甜糖星愿自动采集'), translate('可以帮你每日自动收取甜糖心愿的星愿，并且可以指定日期每周自动提现。填写邀请码853288支持作者！'))
+m = Map(config, translate('甜糖星愿自动采集'), translate('可以帮你每日自动收取甜糖心愿的星愿，并且可以指定日期每周自动提现!'))
 s = m:section(TypedSection, 'global', translate('基本设置'))
 s.anonymous = true
 
@@ -40,18 +40,18 @@ if (isLogin) then
     o.rmempty = false
 
     o = s:option(ListValue, 'week', translate('自动提现日期'))
-    o:value('Monday', translate('星期一'))
-    o:value('Tuesday', translate('星期二'))
-    o:value('Wednesday', translate('星期三'))
-    o:value('Thursday', translate('星期四'))
-    o:value('Friday', translate('星期五'))
-    o:value('Saturday', translate('星期六'))
-    o:value('Sunday ', translate('星期天'))
+    o:value('Monday', translate('Monday'))
+    o:value('Tuesday', translate('Tuesday'))
+    o:value('Wednesday', translate('Wednesday'))
+    o:value('Thursday', translate('Thursday'))
+    o:value('Friday', translate('Friday'))
+    o:value('Saturday', translate('Saturday'))
+    o:value('Sunday', translate('Sunday'))
     o.default = 'Thursday'
     o:depends('auto_cash', '1')
     o.description = translate('请选择每周几作为自动提现日期')
 
-    o = s:option(Value, 'serverchan', translate('Server酱 SCKEY'))
+    o = s:option(Value, 'serverchan', translate('ServerChan SCKEY'))
     o.rmempty = true
     o.description = translate('微信推送，基于Server酱服务，请自行登录 http://sc.ftqq.com/ 绑定并获取 SCKEY ')
 
@@ -59,11 +59,11 @@ if (isLogin) then
 
     o = s:option(Value, 'tg_token', translate('Telegram Bot Token'))
     o.rmempty = true
-    o.description = translate('首先在Telegram上搜索BotFather机器人，创建一个属于自己的通知机器人，并获取Token。')
+    o.description = translate('首先在Telegram上搜索BotFather机器人，创建一个属于自己的通知机器人，并获取Token.')
 
     o = s:option(Value, 'tg_userid', translate('Telegram UserID'))
     o.rmempty = true
-    o.description = translate('在Telegram上搜索getuserIDbot机器人，获取UserID。')
+    o.description = translate('在Telegram上搜索getuserIDbot机器人，获取UserID.')
 
     o = s:option(DummyValue, '', '')
     o.rawhtml = true
